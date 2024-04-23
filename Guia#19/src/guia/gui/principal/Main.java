514ac5ddcf9dfa2c11dec0391049e75dac6a3db3
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-23 15:39:21 -0500

    Layouts y paneles

---
+++ Guia#19/src/guia/gui/principal/Main.java
@@ -0,0 +1,20 @@
+
+package guia.gui.principal;
+
+import guia.gui.views.Window;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+    
+public static void main(String[]args){
+
+Window ventana = new Window();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+}
+    
+}
